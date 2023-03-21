d4cf6336b2c421c0608f10c6eb0221ef2b7712fb
Aaron <abarnard@protonmail.com>
2023-03-21 11:30:08 +1100

    Remove unused code and fix offer_id

--- src/index.js
+++ src/index.js
@@ -9,124 +9,6 @@ const ALPHABET = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l'
 const isBech32 = {}
 const ALPHABET_MAP = {}
 
-class Recurrence {
-  constructor(
-    recurrence,
-    recurrence_paywindow = null,
-    recurrence_limit = null,
-    recurrence_base = null
-  ) {
-    this.period = recurrence['period']
-    this.time_unit = recurrence['time_unit']
-    this.paywindow = recurrence_paywindow
-    this.limit = recurrence_limit
-    this.base = recurrence_base
-  }
-  has_fixed_base() {
-    return this.base != null
-  }
-  can_start_offset() {
-    return this.has_fixed_base && this.base['start_any_period']
-  }
-  _adjust_date(basedate, units, n, sameday) {
-    while (true) {
-      if (units == 'seconds') {
-        var ret = new Date(basedate)
-        ret.setSeconds(ret.getSeconds() + n)
-      } else if (units == 'days') {
-        var ret = new Date(basedate)
-        ret.setDate(ret.getDate() + n)
-      } else if (units == 'months') {
-        var ret = new Date(basedate)
-        ret.setMonth(ret.getMonth() + n)
-      } else if (units == 'years') {
-        var ret = new Date(basedate)
-        ret.setMonth(ret.getMonth() + 12 * n)
-      }
-      if (!sameday || ret.getDate() == basedate.getDate()) return ret
-      basedate.setDate(basedate.getDate() - 1)
-    }
-  }
-  _get_period(n, basetime) {
-    var basedate = new Date()
-    basedate.setTime(basetime * 1000)
-    if (this.time_unit == 0) {
-      var units = 'seconds'
-      var sameday = false
-    } else if (this.time_unit == 1) {
-      var units = 'days'
-      var sameday = false
-    } else if (this.time_unit == 2) {
-      var units = 'months'
-      var sameday = true
-    } else if (this.time_unit == 3) {
-      var units = 'years'
-      var sameday = true
-    }
-
-    var startdate = new Date()
-    startdate.setTime(this._adjust_date(basedate, units, this.period * n, sameday))
-    var enddate = new Date()
-    enddate.setTime(this._adjust_date(startdate, units, this.period, sameday))
-    var start = startdate.getTime()
-    var end = enddate.getTime()
-
-    if (this.paywindow == null) {
-      var paystartdate = this._adjust_date(startdate, units, -this.period, sameday)
-      var paystart = paystartdate.getTime()
-      var payend = end
-    } else {
-      var paystart = start - Number(this.paywindow['seconds_before'])
-      var payend = start + Number(this.paywindow['seconds_after'])
-    }
-    return {
-      start: start / 1000,
-      end: end / 1000,
-      paystart: paystart / 1000,
-      payend: payend / 1000
-    }
-  }
-  get_period(n) {
-    if (this.limit != null && n > this.limit && n <= 0) {
-      return null
-    }
-    if (this.base != null) {
-      var basetime = this.base['basetime']
-    }
-    return this._get_period(n, basetime)
-  }
-  get_pay_factor(period, time) {
-    if (this.paywindow == null || !this.paywindow['proportional_amount']) return 1
-    if (time < period['start']) return 1
-    if (time > period['end']) return 0
-    return (period['end'] - time) / period['end'] - period['start']
-  }
-  period_start_offset(when) {
-    if (this.can_start_offset) {
-      if (this.time_unit == 0) {
-        var approx_mul = 1
-      } else if (this.time_unit == 1) {
-        var approx_mul = 24 * 60 * 60
-      } else if (this.time_unit == 2) {
-        var approx_mul = 30 * 24 * 60 * 60
-      } else if (this.time_unit == 3) {
-        var approx_mul = 365 * 30 * 24 * 60 * 60
-      }
-      var period_num = (when - self.base['basetime']) / (self.period * approx_mul)
-      while (true) {
-        period = self._get_period(period_num, self.base['basetime'])
-        if (when < period['end']) {
-          period_num -= 1
-        } else if (when >= this.period.end) {
-          period_num += 1
-        } else {
-          return period_num
-        }
-      }
-    } else throw Error('can_start_offset is not true')
-  }
-}
-
 for (let z = 0; z < ALPHABET.length; z++) {
   const x = ALPHABET.charAt(z)
   ALPHABET_MAP[x] = z
@@ -491,9 +373,7 @@ function decode(paymentReq) {
     if (tagCode in TAGPARSER) {
       const tagName = TAGPARSER[tagCode][0]
       final[tagName] = TAGPARSER[tagCode][2](Buffer.from(tagWords))
-    }
-    // This doesn't apply to invoices?
-    else if (Number(tagCode) % 2 == 1) {
+    } else if (Number(tagCode) % 2 == 1) {
       unknowns[tagCode] = tagWords
       final[tagcode] = tagWords.toString('hex')
     } else {
@@ -508,9 +388,7 @@ function decode(paymentReq) {
     }
   }
 
-  console.log({ tags })
-
-  final['offer_id'] = merkle_calc(tags)
+  final['offer_id'] = bytesToHex(hash(Buffer.concat(tags.map((tag) => Buffer.from(tag, 'hex')))))
 
   if (prefix == 'lno') {
     if (check_offer(final)) {
@@ -528,96 +406,6 @@ function decode(paymentReq) {
     if (check_invoice_request(final)) {
       final['valid'] = true
       return final
-    }
-  }
-}
-
-function get_recurrence(address) {
-  let decoded = decode(address)
-  if (!'recurrence' in decoded) {
-    return null
-  } else {
-    recur = new Recurrence(
-      decoded['recurrence'],
-      decoded['recurrence_paywindow'],
-      decoded['recurrence_limit'],
-      decoded['recurrence_base']
-    )
-    return recur
-  }
-}
-
-let tlv_invoice_request_rev = {}
-
-for (const [key, value] of Object.entries(tlv_invoice_request)) {
-  tlv_invoice_request_rev[value[0]] = Number(key)
-}
-
-function invoice_req_check(offer, val_dict) {
-  if (!('payer_key' in val_dict)) {
-    throw Error('payer key required!')
-  }
-  // SIGNATURE ENCODE AFTER GETTING THE PAYER_KEY
-  if ('quantity_max' in offer) {
-    if (!('quantity' in val_dict)) {
-      throw Error('Must set quantity')
-    } else {
-      if ('quantity_min' in offer) {
-        if (val_dict['quantity'] < offer['quantity_min']) {
-          throw Error('quantity is less than quantity_min')
-        }
-      }
-      if ('quantity_max' in offer) {
-        if (val_dict['quantity'] > offer['quantity_max']) {
-          throw Error('quantity is greater than quantity_max')
-        }
-      }
-    }
-  } else {
-    if ('quantity' in val_dict) {
-      throw Error('Quantity is not required!')
-    }
-  }
-
-  if (!'amount' in offer) {
-    if (val_dict['amount'] == null) {
-      throw Error('Set amount!')
-    }
-  } else {
-    if ('amount' in val_dict) {
-      if (val_dict['amount'] < offer['amount']) {
-        throw Error('amount is less than what is required!')
-      }
-    }
-  }
-  //previous unpaid invoice(clear doubts from rusty)
-  if ('recurrence' in offer) {
-    if (!val_dict['recurrence_counter'] in val_dict) {
-      throw Error('set a valid recurrence counter')
-    }
-    if ('recurrence_base' in offer && offer['recurrence_base']['start_any_period']) {
-      if (!'recurrence_start' in val_dict) throw Error('Must set valid recurrence_start')
-      if (!'period_offset' in val_dict) throw Error('Must set valid period_offset')
-    } else {
-      if ('recurrence_start' in val_dict) {
-        throw Error('recurrence_start is illegal')
-      }
-    }
-    if ('recurrence_limit' in offer && val_dict['recurrence_counter'] > offer['recurrence_limit']) {
-      throw Error('Invoice request for a period greater than max_period is illegal!')
-    }
-    if ('recurrence_paywindow' in offer) {
-      if ('recurrence_basetime' in offer || 'recurrence_counter' in offer) {
-        //should not send for period prior to 'seconds_before' and later than 'seconds_after'
-      }
-    } else {
-    }
-  } else {
-    if ('recurrence_counter' in val_dict) {
-      throw Error('There is no recurrence in offer!')
-    }
-    if ('recurrence_start' in val_dict) {
-      throw Error('There is no recurrence in offer!')
     }
   }
 }
